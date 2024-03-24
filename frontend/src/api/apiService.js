e8a5890c0afb082ba40d66edc7a6b8795bf7700a
Rimon Hossain <rimonhossen333@gmail.com>
2024-03-25 01:25:16 +0600

    change api base URL

--- frontend/src/api/apiService.js
+++ frontend/src/api/apiService.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 export const apiService = axios.create({
-  baseURL: "http://localhost:3001",
+  // baseURL: "http://localhost:3001",
+  baseURL: "https://e-commerce-mern-project.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
