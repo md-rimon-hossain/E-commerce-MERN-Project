e9f2a24b92be4c5967b4da2439a63d4a7e872502
Rimon Hossain <rimonhossen333@gmail.com>
2024-03-24 16:02:22 +0600

    added render deployed api with localhost

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { Provider } from "react-redux";
@@ -12,9 +12,7 @@ import "./index.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router}/>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-     proxy: {
-      "/api": "http://localhost:3001"
-      // "/api": "https://e-commerce-backend-alpha-ruddy.vercel.app"
-     }
+  server: {
+    proxy: {
+      "/api": "http://localhost:3001",
+      "/api": "https://e-commerce-mern-project.onrender.com"
+    },
   },
   plugins: [react()],
-})
+});
