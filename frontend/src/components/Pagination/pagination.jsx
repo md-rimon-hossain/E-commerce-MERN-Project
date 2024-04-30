14cfa3c40b7568f9482e8561c7ddd1b0b2d69ff0
Rimon Hossain <rimonhossen333@gmail.com>
2024-04-30 12:25:06 +0600

    add pagination don't complete yet

--- frontend/src/components/Pagination/pagination.jsx
+++ frontend/src/components/Pagination/pagination.jsx
@@ -1,107 +1,55 @@
+/* eslint-disable react/prop-types */
 import { ArrowLeft, ArrowRight } from "lucide-react";
 
-const items = [
-  {
-    id: 1,
-    title: "Back End Developer",
-    department: "Engineering",
-    type: "Full-time",
-    location: "Remote",
-  },
-  {
-    id: 2,
-    title: "Front End Developer",
-    department: "Engineering",
-    type: "Full-time",
-    location: "Remote",
-  },
-  {
-    id: 3,
-    title: "User Interface Designer",
-    department: "Design",
-    type: "Full-time",
-    location: "Remote",
-  },
-];
-
-export default function Pagination() {
+export default function Pagination({ pagination }) {
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
       <div className="flex flex-1 justify-between sm:hidden">
-        <button
-          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-        >
+        <button className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
           Previous
         </button>
-        <button
-          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-        >
+        <button className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
           Next
         </button>
       </div>
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div>
           <p className="text-sm text-gray-700">
-            Showing <span className="font-medium">1</span> to{" "}
-            <span className="font-medium">10</span> of{" "}
-            <span className="font-medium">97</span> results
+            Showing <span className="font-medium">1 </span>
+            {/* to{" "}
+                      <span className="font-medium">10</span> */}
+            of{"  "}
+            <span className="font-medium">
+              {pagination?.totalProducts}
+            </span>{" "}
+            results
           </p>
         </div>
         <div>
-          <nav
+          <div
             className="isolate inline-flex -space-x-px rounded-md shadow-sm"
             aria-label="Pagination"
           >
-            <button
-              className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
+            <button className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
               <span className="sr-only">Previous</span>
               {/* <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" /> */}
               <ArrowLeft />
             </button>
             {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
+
             <button
               aria-current="page"
               className="relative z-10 inline-flex items-center bg-[#df6172] px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              1
+              {pagination?.totalPages}
             </button>
-            <button
-              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
-              2
-            </button>
-            <button
-              className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-            >
-              3
-            </button>
-            <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
-              ...
-            </span>
-            <button
-              className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-            >
-              8
-            </button>
-            <button
-              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
-              9
-            </button>
-            <button
-              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
-              10
-            </button>
-            <button
-              className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
-              <span className="sr-only">Next</span>
-              {/* <ChevronRightIcon className="h-5 w-5" aria-hidden="true" /> */}
+
+            <button className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+              <span className="sr-only">Previous</span>
+              {/* <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" /> */}
               <ArrowRight />
             </button>
-          </nav>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,26 +1,22 @@
 import PageTitle from "../components/PageTitle";
-import { apiService } from "../api/apiService";
-import  "./Home.css";
+import "./Home.css";
 import { ProductCard } from "../components/ProductsCard/ProductCard";
 
 import Carousel from "../components/Carousel/Carousel";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllProducts } from "../store/productsSlice";
+import { fetchProductsDataAsync } from "../store/productsSlice";
 import Pagination from "../components/Pagination/pagination";
 
-
 function Home() {
-
-  const { allProducts } = useSelector((state) => state.products);
+  const { allProducts,pagination, loading } = useSelector((state) => state.products);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     (async () => {
       if (!allProducts) {
-        const res = await apiService.get("/api/products");
-        dispatch(getAllProducts(res.data.payload.products));
+        dispatch(fetchProductsDataAsync());
       }
     })();
   }, [dispatch, allProducts]);
@@ -33,12 +29,41 @@ function Home() {
 
         <h2 className="text-3xl font-bold text-[#111827]">SHOES</h2>
         <div className="grid gap-1  xm-grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-2">
-          {allProducts &&
+          {loading ? (
+            <>
+              <div className="flex flex-col gap-4 md:w-60 mb-3">
+                <div className="skeleton h-40 w-full "></div>
+                <div className="skeleton h-4 w-28"></div>
+                <div className="skeleton h-4 w-full"></div>
+                <div className="skeleton h-4 w-full"></div>
+              </div>
+              <div className="flex flex-col gap-4 md:w-60 ">
+                <div className="skeleton h-40 w-full "></div>
+                <div className="skeleton h-4 w-28"></div>
+                <div className="skeleton h-4 w-full"></div>
+                <div className="skeleton h-4 w-full"></div>
+              </div>
+              <div className="flex flex-col gap-4 md:w-60 ">
+                <div className="skeleton h-40 w-full "></div>
+                <div className="skeleton h-4 w-28"></div>
+                <div className="skeleton h-4 w-full"></div>
+                <div className="skeleton h-4 w-full"></div>
+              </div>
+              <div className="flex flex-col gap-4 md:w-60 ">
+                <div className="skeleton h-40 w-full "></div>
+                <div className="skeleton h-4 w-28"></div>
+                <div className="skeleton h-4 w-full"></div>
+                <div className="skeleton h-4 w-full"></div>
+              </div>
+            </>
+          ) : (
+            allProducts &&
             allProducts.map((product, _id) => {
               return <ProductCard key={_id} product={product} />;
-            })}
+            })
+          )}
         </div>
-        <Pagination/>
+        <Pagination pagination={pagination } />
       </div>
     </>
   );
